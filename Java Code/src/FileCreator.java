2cb50db442c2d51c78f337986c84fd9a4de852a0
Nicolas-Mousten <nicolas.mousten@gmail.com>
2022-03-08 15:44:48 +0100

    Coming along

--- Java Code/src/FileCreator.java
+++ Java Code/src/FileCreator.java
@@ -1,5 +1,21 @@
+import java.io.File;
+import java.io.IOException;
+
 public class FileCreator {
     public static void createDDL(String structure, String fileName){
+        try{
+            File DDLFile = new File(fileName);
+            if (DDLFile.createNewFile()) {
+                System.out.println("File created: " + DDLFile.getName());
+            } else {
+                System.out.println("File already exists.");
+            }
+        }catch (IOException e){
+
+        }
+
+        //Create the DDL file as a text File then input the String into the file
+        //Then do the same with SQL Input.
 
     }
 }
