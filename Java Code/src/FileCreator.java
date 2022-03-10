476590079542f3120c3b696342a7ad2346fe234c
Nicolas-Mousten <nicolas.mousten@gmail.com>
2022-03-10 14:43:03 +0100

    Creating DDL file

--- Java Code/src/FileCreator.java
+++ Java Code/src/FileCreator.java
@@ -1,8 +1,7 @@
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 
 public class FileCreator {
-    public static void createDDL(String structure, String fileName){
+    public static void createDDL(String fileName){
         try{
             File DDLFile = new File(fileName);
             if (DDLFile.createNewFile()) {
@@ -11,11 +10,22 @@ public class FileCreator {
                 System.out.println("File already exists.");
             }
         }catch (IOException e){
-
+            System.out.println("An error has occurred");
         }
-
-        //Create the DDL file as a text File then input the String into the file
-        //Then do the same with SQL Input.
-
     }
+
+    public static void fillDDL(String input, String fileNamePath){
+        try(FileWriter fw = new FileWriter(fileNamePath, true);
+            BufferedWriter bw = new BufferedWriter(fw);
+            PrintWriter out = new PrintWriter(bw))
+        {
+            out.println(input);
+            System.out.println("Wrote to file");
+        } catch (IOException e) {
+            System.out.println("Failed to write to file");
+        }
+    }
+
+    //Create the DDL file as a text File then input the String into the file
+    //Then do the same with SQL Input.
 }
--- Java Code/src/RunCode.java
+++ Java Code/src/RunCode.java
@@ -3,28 +3,31 @@ import java.util.ArrayList;
 public class RunCode {
     public static void main(String[] args) {
         TableStructure imdb = CSVReader.csvReader("Java Code/Resources/imdb-data.csv", ";");
-        System.out.println(imdb);
-        String DDL = sqlCommit("Java Code/Resources/imdb-data.csv",
+        //System.out.println(imdb);
+        String DDL = sqlDDL("imdb-data.csv",
                 imdb.getTitle().get(0),
                 imdb.getTitle().get(1),
                 imdb.getTitle().get(2),
                 imdb.getTitle().get(3),
                 imdb.getTitle().get(4),
                 imdb.getTitle().get(5));
-        System.out.println(DDL);
-        FileCreator.createDDL(DDL,"Java Code/SQL/DDL.sql");
-
+        //System.out.println(DDL);
+        FileCreator.createDDL("Java Code/src/SQL/DDL.sql");
+        FileCreator.fillDDL(DDL,"Java Code/src/SQL/DDL.sql");
     }
 
 
-    public static String sqlCommit(String fileName,String yearTitle, String lengthYear, String titleTitle, String subjectTitle, String popularityTitle, String awardsTitle){
-        return  "CREATE TABLE "+ fileName +" {"+
-                yearTitle+" Varchar(255)," +
-                lengthYear+" Varchar(255),"+
-                titleTitle+" Varchar(255),"+
-                subjectTitle+" Varchar(255),"+
-                popularityTitle+" Varchar(255),"+
-                awardsTitle+" Varchar(255)"+
-                "}";
+    public static String sqlDDL(String fileName,String yearTitle, String lengthYear, String titleTitle, String subjectTitle, String popularityTitle, String awardsTitle){
+        return  "CREATE TABLE "+ "`"+fileName+"`" +" (\n"+
+                "`"+yearTitle+"`"+" Varchar(255),\n" +
+                "`"+lengthYear+"`"+" Varchar(255),\n"+
+                "`"+titleTitle+"`"+" Varchar(255),\n"+
+                "`"+subjectTitle+"`"+" Varchar(255),\n"+
+                "`"+popularityTitle+"`"+" Varchar(255),\n"+
+                "`"+awardsTitle+"`"+" Varchar(255)\n"+
+                ");";
+    }
+    public static String sqlDML(String fileName){
+        return "INSERT INTO" + "`" +fileName+ "`" + "";
     }
 }
